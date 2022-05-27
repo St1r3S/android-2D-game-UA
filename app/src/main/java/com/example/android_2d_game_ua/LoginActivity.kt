da20dcb44cf72ebce5f32db929b0e45a0cc720ed
St1r3S <mikmuael.steam@gmail.com>
2022-05-27 21:23:05 +0300

    added registration and authorisation options

---
+++ app/src/main/java/com/example/android_2d_game_ua/LoginActivity.kt
@@ -0,0 +1,77 @@
+package com.example.android_2d_game_ua
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tv_register.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
+        btn_login.setOnClickListener {
+            when {
+                TextUtils.isEmpty(et_login_email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter email!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(et_login_password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter password!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = et_login_email.text.toString().trim { it <= ' ' }
+                    val password: String = et_login_password.text.toString().trim { it <= ' ' }
+
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "You were logged in successfully!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent =
+                                    Intent(this@LoginActivity, MainActivity::class.java)
+                                intent.flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra(
+                                    "user_id",
+                                    FirebaseAuth.getInstance().currentUser!!.uid
+                                )
+                                intent.putExtra(
+                                    "username_id",
+                                    FirebaseAuth.getInstance().currentUser!!.displayName
+                                )
+                                intent.putExtra("email_id", email)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/android_2d_game_ua/MainActivity.kt
+++ app/src/main/java/com/example/android_2d_game_ua/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.android_2d_game_ua
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userId = intent.getStringExtra("user_id")
+        val usernameId = intent.getStringExtra("username_id")
+        val emailId = intent.getStringExtra("email_id")
+
+        tv_user_id.text = "User ID :: $userId"
+        tv_username_id.text = "Username :: $usernameId"
+        tv_email_id.text = "Email ID:: $emailId"
+
+        findViewById<Button>(R.id.btn_logout).setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(
+                this@MainActivity,
+                LoginActivity::class.java
+            ))
+            finish()
+        }
     }
 }
