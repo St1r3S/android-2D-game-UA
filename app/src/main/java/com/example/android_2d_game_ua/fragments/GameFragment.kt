52cc995f29299c49ff92cd1168a7926f6198ecae
St1r3S <mikmuael.steam@gmail.com>
2022-06-11 16:08:08 +0300

    added game implementation

--- app/src/main/java/com/example/android_2d_game_ua/fragments/GameFragment.kt
+++ app/src/main/java/com/example/android_2d_game_ua/fragments/GameFragment.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.android_2d_game_ua.R
 import com.example.android_2d_game_ua.repositories.UserRepository
 import com.example.android_2d_game_ua.view_models.GameViewModel
 import com.example.android_2d_game_ua.view_models.factories.GameViewModelFactory
 import kotlinx.android.synthetic.main.fragment_game.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+
 
 class GameFragment : Fragment() {
     private lateinit var viewModel: GameViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         val view = inflater.inflate(R.layout.fragment_game, container, false)
+        val circleCard = view.findViewById<CardView>(R.id.circleCard)
+        val circleStart = view.findViewById<CardView>(R.id.circleStart)
 
         var defaultScore = 0L
         viewModel = ViewModelProvider(
@@ -29,20 +42,70 @@ class GameFragment : Fragment() {
 
         viewModel.getUser(this)
         viewModel.user.observe(viewLifecycleOwner) {
-            view.tv_best_score.text = "best score: ${it.score}"
+            view.tv_best_score.text = "Best score: ${it.score}"
             val currentUserScore = it.score
             val currentUserUID = it.userId
-            view.findViewById<Button>(R.id.tv_btn_increment).setOnClickListener {
-                defaultScore += 100
-                view.tv_current_score.text = defaultScore.toString()
-            }
-            view.findViewById<Button>(R.id.tv_btn_stop).setOnClickListener {
-                if (defaultScore > (currentUserScore ?: 0L)) {
-                    viewModel.setScore(currentUserUID.toString(), defaultScore)
+
+            val backgroundColorToWin = ContextCompat.getColor(view.context, R.color.yellow)
+            val backgroundColorToLose = ContextCompat.getColor(view.context, R.color.blue)
+            viewLifecycleOwner.lifecycleScope.launch {
+                var loop = true
+                circleStart.setOnClickListener {
+                    circleStart.visibility = View.GONE
+                    circleCard.visibility = View.VISIBLE
+                    loop = true
                 }
-                defaultScore = 0
-                view.tv_current_score.text = defaultScore.toString()
+
+                while (loop) {
+                    withContext(Dispatchers.Main) {
+                        when (Random.nextBoolean()) {
+                            true -> {
+                                circleCard.setCardBackgroundColor(backgroundColorToLose)
+                                view.score_btn_1.text = "Don't click"
+                                delay(
+                                    TimeUnit.MILLISECONDS.toMillis(
+                                        Random.nextLong(
+                                            500,
+                                            1200
+                                        )
+                                    )
+                                )
+                            }
+                            false -> {
+                                circleCard.setCardBackgroundColor(backgroundColorToWin)
+                                view.score_btn_1.text = "Click, NOW"
+                                delay(
+                                    TimeUnit.MILLISECONDS.toMillis(
+                                        Random.nextLong(
+                                            200,
+                                            400
+                                        )
+                                    )
+                                )
+
+                            }
+                        }
+                        circleCard.setOnClickListener {
+                            if (circleCard.cardBackgroundColor.defaultColor == backgroundColorToWin) {
+                                defaultScore += 100
+                                view.tv_current_score.text = defaultScore.toString()
+                                circleCard.setCardBackgroundColor(backgroundColorToLose)
+                            } else {
+                                if (defaultScore > (currentUserScore ?: 0L)) {
+                                    viewModel.setScore(currentUserUID.toString(), defaultScore)
+                                }
+                                defaultScore = 0
+                                view.tv_current_score.text = defaultScore.toString()
+                                loop = false
+                                circleStart.visibility = View.VISIBLE
+                                circleCard.visibility = View.GONE
+                            }
+                        }
+                    }
+                }
+
             }
+
         }
 
         return view
